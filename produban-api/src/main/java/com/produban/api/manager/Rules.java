1b25d323795a3e6b6e9cdb5923b3396ad94a0c57
guillermoortiz <guillermo.ortiz.fdez@gmail.com>
2015-02-06 09:24:13 +0100

    Added rules

---
+++ produban-api/src/main/java/com/produban/api/manager/Rules.java
@@ -0,0 +1,14 @@
+package com.produban.api.manager;
+
+import java.util.List;
+
+import com.produban.api.data.Rule;
+
+/**
+ * Interface to define the methods
+ * @author ortizg1
+ *
+ */
+interface Rules {
+	public List<Rule> getRules();
+}

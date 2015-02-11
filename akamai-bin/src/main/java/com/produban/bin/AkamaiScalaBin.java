bee5c6a9c06ef535627cc99a46966b155914a719
guillermoortiz <guillermo.ortiz.fdez@gmail.com>
2015-02-11 11:15:26 +0100

    added templace ES

---
+++ akamai-bin/src/main/java/com/produban/bin/AkamaiScalaBin.java
@@ -0,0 +1,27 @@
+package com.produban.bin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.produban.api.data.Rule;
+import com.produban.api.general.Factory;
+import com.produban.api.manager.ConfigurationManager;
+import com.produban.api.manager.RulesManager;
+import com.produban.starkstreaming.AkamaiKafkaStreaming;
+
+public class AkamaiScalaBin {
+	public static void main(String[] args) {
+		ConfigurationManager configurationManager = Factory
+				.getConfigurationManager();
+		RulesManager rulesManager = Factory.getRulesManager();
+		HashMap<String, String> configurations = (HashMap<String, String>) configurationManager
+				.getMapConfigurations();
+		ArrayList<Rule> rules = (ArrayList<Rule>) rulesManager.getRules();
+
+		System.out.println("Starting Streaming..");
+		AkamaiKafkaStreaming akamaiStreaming = new AkamaiKafkaStreaming(
+				configurations, rules);
+		akamaiStreaming.startStreaming();
+		System.out.println("Ending Streaming..");
+	}
+}
---
+++ akamai-bin/src/scripts/akamaiStreaming-scala.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export HADOOP_CONF_DIR=/etc/hadoop/conf
+SPARK_CLASSPATH=""
+for lib in `ls /user/etc/lib/*.jar`
+do
+        if [ -z "$SPARK_CLASSPATH" ]; then
+		SPARK_CLASSPATH=$lib
+	else
+		SPARK_CLASSPATH=$lib,$SPARK_CLASSPATH
+	fi
+done
+
+spark-submit --name "Akamai Streaming Scala" --master yarn-client --class com.produban.bin.AkamaiScalaBin --jars $SPARK_CLASSPATH --executor-memory 1g /user/etc/executor/produban-akamai-bin.jar
